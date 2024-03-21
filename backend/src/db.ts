e89a72351d2b007dd686edb2c5c8a8b3a80cc6d3
squiddy <liuwenkai2017@gmail.com>
2024-03-21 14:05:52 +0800

    update service names

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise"
 
 const connection = await mysql.createConnection({
-    host: "db",
+    host: "peerly-db",
     user: "peerly",
     password: "peerly",
     database: "peerly",
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,7 +32,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         store: new MemcachedStore({
-            hosts: ["memcached:11211"],
+            hosts: ["peerly-memcached:11211"],
             secret: settings.MEMCACHED_SECRET,
         }),
     }),
