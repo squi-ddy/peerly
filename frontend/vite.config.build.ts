d62360d45baf9cf449e52e940b1118b8f77ebbd4
squiddy <squid2015guy@gmail.com>
2026-06-12 16:48:08 +0800

    fix tailwind config

--- frontend/vite.config.build.ts
+++ frontend/vite.config.build.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import typescript from "@rollup/plugin-typescript"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
             tsconfig: "./tsconfig.build.json",
             include: ["./src/**/*.ts", "./src/**/*.tsx"],
         }),
+        tailwindcss(),
     ],
     esbuild: false,
     build: {
